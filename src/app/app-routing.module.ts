049ea0e9f0bbe29f0859e990d7c2f1d7dc914965
satoshi.watanabe@heart.co.jp <satoshi.watanabe@heart.co.jp>
2021-09-03 07:19:11 +0900

    リリース環境で、ブラウザの再更新ができないため。useHashを有効化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{path:'question',component:QuestionComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
