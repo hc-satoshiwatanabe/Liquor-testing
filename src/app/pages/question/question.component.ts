2b28eca73a2fbe9a2c777627b1312e39836559dd
satoshi.watanabe@heart.co.jp <satoshi.watanabe@heart.co.jp>
2021-09-12 00:46:18 +0900

    シャッフルした際の問題番号が飛んでしまう現象を修正

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -3,7 +3,6 @@ import { itca } from '../data/data_itca';
 import { Question } from '../data/model';
 import { nihonsyu3 } from '../data/data_nihonsyu3';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
@@ -34,13 +33,7 @@ export class QuestionComponent implements OnInit {
   }
 
 
-  private shuffle(array:any) {
-    for (let i = array.length - 1; i >= 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
+
 
   public openDialog() {
 
@@ -86,36 +79,51 @@ export class QuestionComponent implements OnInit {
 
   }
 
-  private SetQuestions(p_ar: Question[]) {
+  private SetQuestions(p_Ar: Question[]) {
 
     this.questions = [];
 
+    let wi: number = 0;
+
     //シャッフル有無
     if (this.isShuffle) {
-      let ar: number[] = [...Array(p_ar.length)].map((_, i) => i); //=> [ 0, 1, 2, 3, 4 ]
 
-      ar = this.shuffle(ar);
-      for (let i = 0; i < p_ar.length; i++) {
-        for (let ii = 0; ii < ar.length; ii++) {
-          this.questions[i] = p_ar[ar[i]];
+      //要素数分の配列を作成
+      let randomAr: number[] =  [...Array(p_Ar.length)].map((_, i) => i); //=> [ 0, 1, 2, 3, 4 ]
+
+      //要素番号をシャッフル
+      randomAr = this.shuffle(randomAr);
+
+      //問題文に代入
+      for (let i = 0; i < p_Ar.length; i++) {
+        if (p_Ar[randomAr[i]].chapter !== '') {
+          this.questions[wi] = p_Ar[randomAr[i]];
+          wi++;
         }
       }
     }
     else {
-      this.questions = p_ar;
+      //事前登録のデータ順
+      this.questions = p_Ar;
 
     }
   }
-
-
-private HasSelected() :Boolean {
-  for (let i = 0; i < this.questions.length; i++) {
-    if (this.selectItem[i] !== undefined) {
-      return true;
+  private shuffle(array: any) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
   }
-  return false;
-}
+
+  private HasSelected() :Boolean {
+    for (let i = 0; i < this.questions.length; i++) {
+      if (this.selectItem[i] !== undefined) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 public myCheck() {
     for (let i = 0; i < this.questions.length; i++) {
