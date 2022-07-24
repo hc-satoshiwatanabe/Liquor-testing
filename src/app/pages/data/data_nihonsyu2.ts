f35d9a93070a3e9d2a806f13ef24fb379247a4b2
satoshi.watanabe@heart.co.jp <satoshi.watanabe@heart.co.jp>
2022-07-24 21:06:53 +0900

    日本酒検定２級の問題文の追加

--- src/app/pages/data/data_nihonsyu2.ts
+++ src/app/pages/data/data_nihonsyu2.ts
@@ -103,6 +103,160 @@ export const nihonsyu2: Question[] = [
     "tips": ""
   },
   {
+    "chapter": "日本酒で使用される黄麹菌は次のうちどれか。",
+    "items": [
+      { "val": 0, "name": "アスペルギルス・リュウキュウエンシス" },
+      { "val": 1, "name": "アスペルギルス・カワチ" },
+      { "val": 2, "name": "アスペルギルス・リゾープス" },
+      { "val": 3, "name": "アスペルギルス・オリゼ" }
+    ],
+    "answer": [3],
+    "tips": ""
+  },
+  {
+    "chapter": "製麹の工程において床作業と呼ばれる工程に当てはまらないものはどれか。",
+    "items": [
+      { "val": 0, "name": "引き込み" },
+      { "val": 1, "name": "仲仕事" },
+      { "val": 2, "name": "盛り" },
+      { "val": 3, "name": "切り返し" }
+    ],
+    "answer": [1],
+    "tips": ""
+  },
+  {
+    "chapter": "市場に出回る商品の中で速醸系酵母を用いて造られる日本酒の割合は、どれくらいとされているか",
+    "items": [
+      { "val": 0, "name": "約90%" },
+      { "val": 1, "name": "約80%" },
+      { "val": 2, "name": "約70%" },
+      { "val": 3, "name": "約60%" }
+    ],
+    "answer": [0],
+    "tips": ""
+  },
+  {
+    "chapter": "1953年に熊本県酒造研究所で分離された非常に華やかな吟醸香をもたらす協会酵母はどれか",
+    "items": [
+      { "val": 0, "name": "協会６号" },
+      { "val": 1, "name": "協会７号" },
+      { "val": 2, "name": "協会９号" },
+      { "val": 3, "name": "協会１８０１号" }
+    ],
+    "answer": [2],
+    "tips": ""
+  },
+  {
+    "chapter": "日本酒に添加する醸造アルコールの原料ではないものはどれか",
+    "items": [
+      { "val": 0, "name": "ブドウ" },
+      { "val": 1, "name": "米" },
+      { "val": 2, "name": "糖蜜類" },
+      { "val": 3, "name": "トウモロコシ" }
+    ],
+    "answer": [1],
+    "tips": ""
+  },
+  {
+    "chapter": "発酵が始まってすぐの状態の醪を指す言葉はどれか",
+    "items": [
+      { "val": 0, "name": "落泡" },
+      { "val": 1, "name": "筋泡" },
+      { "val": 2, "name": "玉泡" },
+      { "val": 3, "name": "高泡" }
+    ],
+    "answer": [2],
+    "tips": ""
+  },
+  {
+    "chapter": "日本酒の上層後のタンク下部の下呑から抽出した日本酒をなんというか。",
+    "items": [
+      { "val": 0, "name": "滓酒" },
+      { "val": 1, "name": "にごり酒" },
+      { "val": 2, "name": "あらばしり" },
+      { "val": 3, "name": "雫酒" }
+    ],
+    "answer": [0],
+    "tips": ""
+  },
+  {
+    "chapter": "日本酒の製造工程における上槽と同時に瓶詰めする手法をなんと呼ぶか",
+    "items": [
+      { "val": 0, "name": "直汲み" },
+      { "val": 1, "name": "中汲み" },
+      { "val": 2, "name": "瓶囲い" },
+      { "val": 3, "name": "生貯蔵" }
+    ],
+    "answer": [0],
+    "tips": ""
+  },
+  {
+    "chapter": "播磨国風土記に記載されている酒（日本酒）の説明で正しいものはどれか。",
+    "items": [
+      { "val": 0, "name": "今の清酒に近い淡麗辛口の酒が造られていたこと" },
+      { "val": 1, "name": "炊いた米にカビが生えて、それば水に濡れて自然と酒ができたこと" },
+      { "val": 2, "name": "干し飯が水に濡れて発生したカビを用いて酒を造らせたこと" },
+      { "val": 3, "name": "水に浸かった米から自然と発酵したこと" }
+    ],
+    "answer": [2],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
     "chapter": "",
     "items": [
       { "val": 0, "name": "" },
@@ -179,4 +333,115 @@ export const nihonsyu2: Question[] = [
     "answer": [],
     "tips": ""
   },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+  {
+    "chapter": "",
+    "items": [
+      { "val": 0, "name": "" },
+      { "val": 1, "name": "" },
+      { "val": 2, "name": "" },
+      { "val": 3, "name": "" }
+    ],
+    "answer": [],
+    "tips": ""
+  },
+
 ];
